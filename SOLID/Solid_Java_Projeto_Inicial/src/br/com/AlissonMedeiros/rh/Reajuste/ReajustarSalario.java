063712a4b7decb8da26b07eff767726901a8485c
Alisson Medeiros <alisilva1@gmail.com>
2021-01-02 21:40:13 -0300

    Finish

--- SOLID/Solid_Java_Projeto_Inicial/src/br/com/AlissonMedeiros/rh/Reajuste/ReajustarSalario.java
+++ SOLID/Solid_Java_Projeto_Inicial/src/br/com/AlissonMedeiros/rh/Reajuste/ReajustarSalario.java
@@ -18,7 +18,20 @@ public class ReajustarSalario {
     }
 
     public void ReajustarSalarioFuncionario(Funcionario funcionario, BigDecimal aumento){
+
         this.validacoes.forEach(v -> v.Validar(funcionario,aumento));
+
+        /*
+        é a mesma coisa do código acima   1) private List da Interface / 2) Constructor / 3) forEach;
+
+        ValidacaoPercentual validacaoPercentual = new ValidacaoPercentual();
+        ValidacaoPeriodicidade validacaoPeriodicidade = new ValidacaoPeriodicidade();
+
+        validacaoPercentual.validar(funcionario,aumento);
+        validacaoPeriodicidade.validar(funcionario,aumento);
+         */
+
+
         BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.atualizaSalario(salarioReajustado);
 
